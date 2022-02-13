34b9791497e4841801f951a373c5d3352bcf2c27
angjaay <angga.angzai@gmai.com>
2022-02-13 13:30:16 +0700

    issue#redesign-new-thread

--- resources/views/pages/threads/create.blade.php
+++ resources/views/pages/threads/create.blade.php
@@ -4,9 +4,25 @@
         <x-partials.sidenav />
 
         <section class="flex flex-col col-span-3 gap-y-4">
-            <small class="text-sm text-gray-400">discussion>create</small>
+           
+            {{-- breadcrumb --}}
+            <div class="flex items-center pb-2 overflow-y-auto whitespace-nowrap">
+                <a href="#" class="text-gray-600 dark:text-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                    </svg>
+                </a>
+        
+                <span class="mx-5 text-gray-500 dark:text-gray-300">
+                    <x-heroicon-s-chevron-right class="w-5 h-5" />
+                </span>
+        
+                <a href="#" class="text-yellow-500 dark:text-gray-200 ">
+                    Buat Postingan
+                </a>  
+            </div>
 
-            <article class="p-5 bg-white shadow">
+            <article class="p-5 bg-white shadow rounded-lg">
                 <div class="w-full">
 
                     {{-- Create --}}
@@ -16,16 +32,16 @@
 
                                 {{-- Title --}}
                                 <div>
-                                    <x-form.label for="title" value="{{ __('Title') }}" />
+                                    <x-form.label for="title" value="{{ __('Judul') }}" />
                                     <x-form.input id="title" class="block w-full mt-1" type="text" name="title" :value="old('title')" autofocus />
                                     <x-form.error for="title" />
                                 </div>
 
                                 {{-- Category --}}
                                 <div>
-                                    <x-form.label for="category" value="{{ __('Category') }}" />
+                                    <x-form.label for="category" value="{{ __('Kategori') }}" />
                                     <select name="category" id="category" class="w-full text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
-                                        <option value="">Select Category</option>
+                                        <option value="">Pilih Kategori</option>
                                         @foreach ($categories as $category)
                                         <option value="{{ $category->id() }}">{{ $category->name() }}</option>
                                         @endforeach
@@ -46,14 +62,14 @@
 
                                 {{-- Body --}}
                                 <div>
-                                    <x-form.label for="body" value="{{ __('Description') }}" />
+                                    <x-form.label for="body" value="{{ __('Deskripsi') }}" />
                                     <x-trix name="body" styling="shadow-inner bg-gray-100 h-56" />
                                     <x-form.error for="body" />
                                 </div>
 
                                 {{-- Button --}}
                                 <x-buttons.primary>
-                                    {{ __('Create') }}
+                                    {{ __('Buat') }}
                                 </x-buttons.primary>
                         </x-form>
                     </div>
