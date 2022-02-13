a61b5f1dc4d0a5b130ed1cf6770c80fbcc106ff5
angjaay <angga.angzai@gmai.com>
2022-02-13 16:21:42 +0700

    issue#redesign-new-thread

--- resources/views/pages/threads/edit.blade.php
+++ resources/views/pages/threads/edit.blade.php
@@ -4,9 +4,31 @@
         <x-partials.sidenav />
 
         <section class="flex flex-col col-span-3 gap-y-4">
-            <small class="text-sm text-gray-400">threads>{{ $thread->title() }}>edit</small>
+           
+             {{-- breadcrumb --}}
+             <div class="flex items-center pb-2 overflow-y-auto whitespace-nowrap">
+                <a href="#" class="text-gray-600 dark:text-gray-200">
+                    Postingan
+                </a>
+        
+                <span class="mx-5 text-gray-500 dark:text-gray-300">
+                    <x-heroicon-s-chevron-right class="w-5 h-5" />
+                </span>
+        
+                <span class="text-gray-500 dark:text-gray-200 ">
+                    {{ $thread->title() }}
+                </span>  
 
-            <article class="p-5 bg-white shadow">
+                <span class="mx-4 text-gray-500 dark:text-gray-300">
+                    <x-heroicon-s-chevron-right class="w-5 h-5" />
+                </span>
+
+                <span class=" text-yellow-500 dark:text-gray-300">
+                    Edit
+                </span>
+            </div>
+
+            <article class="p-5 bg-white shadow rounded-lg">
                 <div class="w-full">
 
                     {{-- Create --}}
@@ -16,14 +38,14 @@
 
                                 {{-- Title --}}
                                 <div>
-                                    <x-form.label for="title" value="{{ __('Title') }}" />
+                                    <x-form.label for="title" value="{{ __('Judul') }}" />
                                     <x-form.input id="title" class="block w-full mt-1" type="text" name="title" :value="$thread->title()" autofocus />
                                     <x-form.error for="title" />
                                 </div>
 
                                 {{-- Category --}}
                                 <div>
-                                    <x-form.label for="category" value="{{ __('Category') }}" />
+                                    <x-form.label for="category" value="{{ __('Kategori') }}" />
                                     <select name="category" id="category" class="w-full text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                                         @foreach ($categories as $category)
                                         <option value="{{ $category->id() }}" @if($category->id() == $selectedCategory->id) selected @endif>
@@ -34,23 +56,12 @@
                                     <x-form.error for="category" />
                                 </div>
 
-                                {{-- Tags --}}
-                                <div>
-                                    <x-form.label for="tags" value="{{ __('Tags') }}" />
-                                    <select name="tags[]" id="tags" x-data="{}" x-init="function () { choices($el) }" class="w-full text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" multiple>
-                                        @foreach ($tags as $tag)
-                                        <option value="{{ $tag->id() }}" @if(in_array($tag->id(), $oldTags)) selected @endif
-                                            >
-                                            {{ $tag->name() }}
-                                        </option>
-                                        @endforeach
-                                    </select>
-                                    <x-form.error for="tags" />
-                                </div>
+                               
+                        
 
                                 {{-- Body --}}
                                 <div>
-                                    <x-form.label for="body" value="{{ __('Description') }}" />
+                                    <x-form.label for="body" value="{{ __('Deskripsi') }}" />
                                     <x-trix name="body" styling="shadow-inner bg-gray-100 h-56">
                                         {{ $thread->body() }}
                                     </x-trix>
@@ -59,7 +70,7 @@
 
                                 {{-- Button --}}
                                 <x-buttons.primary>
-                                    {{ __('Update') }}
+                                    {{ __('Simpan') }}
                                 </x-buttons.primary>
                         </x-form>
                     </div>
