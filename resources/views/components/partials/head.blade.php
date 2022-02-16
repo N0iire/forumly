369c16b5c85db0ea362a49ed9c4c10be0f8d5815
angjaay <angga.angzai@gmai.com>
2022-02-16 13:46:08 +0700

    fix bootsrtap

--- resources/views/components/partials/head.blade.php
+++ resources/views/components/partials/head.blade.php
@@ -19,12 +19,11 @@
 <!-- Styles -->
 <link rel="stylesheet" href="{{ asset('css/choices.css') }}">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
 
 {{-- Livewire --}}
 <livewire:styles />
